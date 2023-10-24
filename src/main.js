e9487bc5040f65823402066a40d22c13675fa7ac
RahaZaman <rahamatzaman@gmail.com>
2023-10-23 23:59:08 -0700

    Setting up Explanatory Comment for Project

--- src/main.js
+++ src/main.js
@@ -1,3 +1,23 @@
+/*
+
+Rahamat Zaman
+
+Rocket Patrol Transmutations
+
+Approximate time to complete project: 
+
+Mods Chosen: 
+New enemy Spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (5 points)
+Mouse control for player movement and mouse click to fire (5 points)
+New title screen (e.g., new artwork, typography, layout) (3 points)
+Displaying the time remaining (in seconds) on the screen (3 points)
+4 new explosion sound effects at random on impact (3 points)
+Speed increase that happens after 30 seconds in the original game (1 points)
+
+Citations:
+
+*/
+
 let config = {
     type: Phaser.CANVAS,
     width: 640,
